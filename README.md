4ea8898bad42e536436561757cb65d1832907461
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-20 14:26:06 +0200

    First commit with working code

---
+++ README.md
@@ -0,0 +1,7 @@
+# vue-hover-picture
+
+## Project idea
+
+A simple code example of how to change your picture `src` when hovering over it.
+
+Part of my dev.to [article](https://dev.to/chadriae/change-picture-on-hover-with-vue-25lb).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <img :src="pictureHover" @mouseover="hover = true" @mouseleave="hover = false">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      pictureStatic: require("../assets/todo.png"),
+      pictureGif: require("../assets/todo.gif"),
+      hover: false
+    }
+  },
+  computed: {
+    pictureHover () {
+      if (this.hover == true) {
+        return this.pictureGif
+      } else {
+        return this.pictureStatic
+      }
+    }
+  }
+}
+</script>
+
+<style scoped> 
+img {
+  height: 500px;
+}
+</style>
